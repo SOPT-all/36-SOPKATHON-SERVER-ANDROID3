a5298df6087507e6b69819f89f707696209eb82e
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-16 23:39:17 +0900

    init : 초기세팅

---
+++ src/test/java/sopt/android3/sopkathon/SopkathonApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.android3.sopkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
