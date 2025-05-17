55e9a559f11a2f798b050e11943ea2e047a115db
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-18 03:58:43 +0900

    feat : recommend 부분 개발 #2

--- src/main/java/sopt/android3/sopkathon/recommend/domain/Recommend.java
+++ src/main/java/sopt/android3/sopkathon/recommend/domain/Recommend.java
@@ -1,29 +1,32 @@
 package sopt.android3.sopkathon.recommend.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import sopt.android3.sopkathon.recipe.domain.Recipe;
 
+@Getter
 @Entity
 public class Recommend {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long recommend_id;
+	private Long recommendId;
 
-	private String recommend_img;
+	private String recommendImg;
 
-	private String recommend_store;
+	private String recommendStore;
 
-	private String recommend_store_url;
+	private String recommendStoreUrl;
 
-	private Long recommend_item_price;
+	private Long recommendItemPrice;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "recipe_id", nullable = false)
 	private Recipe recipe;
 }
